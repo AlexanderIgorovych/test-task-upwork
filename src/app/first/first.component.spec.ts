43982bfd86d5c83a00956e19397789f22111a1a0
AlexanderIgorovych <sanya.romanyak@gmail.com>
2019-04-11 17:29:36 +0300

    added unit tests

--- src/app/first/first.component.spec.ts
+++ src/app/first/first.component.spec.ts
@@ -1,16 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FirstComponent } from './first.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { SecondComponent } from '../second/second.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { StoreModule, Store  } from '@ngrx/store';
+import { counterReducerLower, counterReducerHigher, initialStateLower, initialStateHigher } from '../store/reducers/counter-reducers';
+import * as actions from '../store/actions/counter-actions';
 
 describe('FirstComponent', () => {
   let component: FirstComponent;
   let fixture: ComponentFixture<FirstComponent>;
 
+  let store: Store<any>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FirstComponent ]
+      declarations: [ FirstComponent, SecondComponent],
+      imports: [
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        StoreModule.forRoot({
+          countlower: counterReducerLower,
+          counthigher: counterReducerHigher
+        })],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
+
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -22,4 +46,39 @@ describe('FirstComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initiate value -5 and 10', () => {
+    const actualMinusFire = counterReducerLower(initialStateLower, {type: 'InitialLower'});
+    const expectedMinusFive = -5;
+    expect(actualMinusFire).toBe(expectedMinusFive);
+
+    const actualPlusTen = counterReducerLower(initialStateHigher, {type: 'InitialHigher'});
+    const expectedPlusTen = 10;
+    expect(actualPlusTen).toBe(expectedPlusTen);
+  });
+
+  it('should have correct types for actions', () => {
+
+    const expectedIncrementLower = new actions.IncrementLower().type;
+    const expectedDecrementLower = new actions.DecrementLower().type;
+    const expectedIncrementHigher = new actions.IncrementHigher().type;
+    const expectedDecrementHigher = new actions.DecrementHigher().type;
+    const expectIncrementHigherTwice = new actions.IncrementHigherTwice().type;
+
+    const expectedInitialLower = new actions.InitialLower().type;
+    const expectedInitialHigher  = new actions.InitialHigher().type;
+    const expectedReset = new actions.Reset().type;
+    const expectedStop = new actions.Stop().type;
+
+    expect(new actions.IncrementLower().type).toEqual(expectedIncrementLower);
+    expect(new actions.DecrementLower().type).toEqual(expectedDecrementLower);
+    expect(new actions.IncrementHigher().type).toEqual(expectedIncrementHigher);
+    expect(new actions.DecrementHigher().type).toEqual(expectedDecrementHigher);
+    expect(new actions.IncrementHigherTwice().type).toEqual(expectIncrementHigherTwice);
+    expect(new actions.InitialLower().type).toEqual(expectedInitialLower);
+    expect(new actions.InitialHigher().type).toEqual(expectedInitialHigher);
+    expect(new actions.Reset().type).toEqual(expectedReset);
+    expect(new actions.Stop().type).toEqual(expectedStop);
+  });
+
 });
--- src/app/second/second.component.spec.ts
+++ src/app/second/second.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SecondComponent } from './second.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FirstComponent } from '../first/first.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { StoreModule } from '@ngrx/store';
+import { counterReducerLower, counterReducerHigher } from '../store/reducers/counter-reducers';
 
 describe('SecondComponent', () => {
   let component: SecondComponent;
@@ -8,7 +16,17 @@ describe('SecondComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SecondComponent ]
+      declarations: [ SecondComponent, FirstComponent],
+      imports: [
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        StoreModule.forRoot({
+          countlower: counterReducerLower,
+          counthigher: counterReducerHigher
+        })],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -22,4 +40,5 @@ describe('SecondComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
